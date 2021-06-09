7a0fb31c3d65886a986acc7ab85014cfb6f99230
fredrik.julin <fredrik.julin@paradoxinteractive.com>
2021-06-09 16:43:16 +0200

    Add player and shooting

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+pub mod frame;
+pub mod player;
+pub mod render;
+pub mod shot;
+
+pub const NUM_ROWS: usize = 20;
+pub const NUM_COLS: usize = 40;
+pub const NUM_SHOTS_ALLOWED: usize = 5;
+
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,92 @@
 use std::error::Error;
 use rusty_audio::Audio;
-use crossterm::Result;
+use crossterm::{Result, terminal, ExecutableCommand, event};
+use std::{io, thread};
+use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
+use crossterm::cursor::{Hide, Show};
+use std::time::{Duration, Instant};
+use crossterm::event::{Event, KeyCode};
+use std::sync::mpsc;
+use space_invaders::{frame, render};
+use space_invaders::player::Player;
+use space_invaders::frame::Drawable;
 
-fn main() -> Result<()>{
-
+fn main() -> Result<()> {
     let mut audio = Audio::new();
+    audio.add("explode", "assets/sounds/explode.wav");
+    audio.add("lose", "assets/sounds/lose.wav");
+    audio.add("move", "assets/sounds/move.wav");
+    audio.add("pew", "assets/sounds/pew.wav");
+    audio.add("startup", "assets/sounds/startup.wav");
+    audio.add("win", "assets/sounds/win.wav");
+    audio.play("startup");
+
+    // Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // Setup Render loop in seperate thread
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+        loop {
+            let curr_frame = match render_rx.recv() {
+                Ok(x) => x,
+                Err(_) => break,
+            };
+            render::render(&mut stdout, &last_frame, &curr_frame, false);
+            last_frame = curr_frame;
+        }
+    });
+
+    // Game Loop
+    let mut player = Player::new();
+    let mut instant = Instant::now();
+    'game_loop: loop {
+        // Per-frame init
+        let delta = instant.elapsed();
+        instant = Instant::now();
+        let mut curr_frame = frame::new_frame();
+        // Input
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Left =>  player.move_left(),
+                    KeyCode::Right => player.move_right(),
+                    KeyCode::Char(' ') => {
+                        if player.shoot() {
+                            audio.play("pew");
+                        }
+                    }
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        break 'game_loop;
+                    }
+                    _ => {}
+                }
+            }
+        }
+
+        // Updates
+        player.update(delta);
+
+        // Draw & render
+        player.draw(&mut curr_frame);
+        let _ = render_tx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    // Cleanup
+    drop(render_tx);
+    render_handle.join().unwrap();
+
+    audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
     Ok(())
 }
